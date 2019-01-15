594b8fdf9e22dca0c1e13ee1b7fba615771bd5c4
steveoaustin <steveoaustin812@gmail.com>
2019-01-15 13:33:13 -0500

    Updates

---
+++ precache-manifest.7f8211c9041b6a2ea1d9232f579c5e0c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "e23fb8181c62a369a8bf6e9a427cd2e0",
+    "url": "/Connect-Four/static/media/human.e23fb818.png"
+  },
+  {
+    "revision": "724e3bc316a01fa5679c2196cdf9029c",
+    "url": "/Connect-Four/static/media/computer.724e3bc3.png"
+  },
+  {
+    "revision": "925710a69404e0074c12",
+    "url": "/Connect-Four/static/js/runtime~main.925710a6.js"
+  },
+  {
+    "revision": "785ceab67af163efcdc4",
+    "url": "/Connect-Four/static/js/main.785ceab6.chunk.js"
+  },
+  {
+    "revision": "7f75d124d6dd0f3019bb",
+    "url": "/Connect-Four/static/js/1.7f75d124.chunk.js"
+  },
+  {
+    "revision": "785ceab67af163efcdc4",
+    "url": "/Connect-Four/static/css/main.6fd4c3df.chunk.css"
+  },
+  {
+    "revision": "04611652dcb601a27207d281485c11d8",
+    "url": "/Connect-Four/index.html"
+  }
+];
