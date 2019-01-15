d14cdc190049f5bd11a751567b3dec9a02606b43
steveoaustin <steveoaustin812@gmail.com>
2019-01-15 17:07:39 -0500

    update board state for turns and fix overlay rendering issue

--- src/board.tsx
+++ src/board.tsx
@@ -47,16 +47,39 @@ export default class Board extends Component<props> {
       }
     }
 
-    if (row >= gameHeight) {
+    if (row < 0) {
       return; // row is full
     }
-    d3.select("#inputOverlay")
+
+    d3.select("#Board")
+      .append("circle")
+      .attr("id", "piece" + this.state.turn)
+      .attr("fill", this.state.turn % 2 === 1 ? "red" : "black")
+      .attr("cx", column * sectionSize + sectionSize / 2 + margin)
+      .attr("cy", sectionSize / 2)
+      .attr("r", pieceSize / 2);
+
+    let newBoard = this.state.board;
+    newBoard[row][column] =
+      this.state.turn % 2 === 1 ? label.player1 : label.player2;
+
+    d3.select("#piece" + this.state.turn)
       .transition()
       .duration(150 + 100 * row)
       .ease(d3.easeBounce)
       .attr("cx", column * sectionSize + sectionSize / 2 + margin)
       .attr("cy", row * sectionSize + sectionSize / 2 + sectionSize + margin)
       .attr("r", pieceSize / 2);
+
+    // swap the color of input overlay to cause "instant" transition
+    d3.select("#inputOverlay").attr(
+      "fill",
+      (this.state.turn + 1) % 2 === 1 ? "red" : "black"
+    );
+
+    this.setState((state: props) => {
+      return { board: newBoard, turn: state.turn + 1 };
+    });
   };
 
   getColumn = (event: React.MouseEvent) => {
@@ -75,28 +98,7 @@ export default class Board extends Component<props> {
     );
   };
 
-  componentDidMount() {
-    d3.select("#Board")
-      .append("rect")
-      .attr("x", 0)
-      .attr("y", sectionSize * topInterfaceHeight)
-      .attr("width", sectionSize * gameWidth + margin * 2)
-      .attr("height", sectionSize * gameHeight + margin * 2)
-      .attr("fill", "yellow");
-
-    // draw lines indicating input areas
-    for (let x = 0; x <= gameWidth; x++) {
-      d3.select("#Board")
-        .append("line")
-        .attr("x1", x * sectionSize + margin)
-        .attr("x2", x * sectionSize + margin)
-        .attr("y1", sectionSpacing)
-        .attr("y2", topInterfaceHeight * sectionSize)
-        .attr("stroke-width", "2")
-        .attr("stroke-linecap", "round")
-        .attr("stroke", "SteelBlue");
-    }
-
+  drawBoard() {
     let currentRow: number = 0;
     let currentColumn: number = 0;
     const board: label[][] = this.state.board as label[][];
@@ -126,6 +128,31 @@ export default class Board extends Component<props> {
       currentColumn = 0;
       currentRow++;
     });
+  }
+
+  componentDidMount() {
+    d3.select("#Board")
+      .append("rect")
+      .attr("x", 0)
+      .attr("y", sectionSize * topInterfaceHeight)
+      .attr("width", sectionSize * gameWidth + margin * 2)
+      .attr("height", sectionSize * gameHeight + margin * 2)
+      .attr("fill", "yellow");
+
+    // draw lines indicating input areas
+    for (let x = 0; x <= gameWidth; x++) {
+      d3.select("#Board")
+        .append("line")
+        .attr("x1", x * sectionSize + margin)
+        .attr("x2", x * sectionSize + margin)
+        .attr("y1", sectionSpacing)
+        .attr("y2", topInterfaceHeight * sectionSize - sectionSpacing)
+        .attr("stroke-width", "2")
+        .attr("stroke-linecap", "round")
+        .attr("stroke", "SteelBlue");
+    }
+
+    this.drawBoard();
 
     // add placeholder circle for input overlay
     d3.select("#Board")
@@ -142,6 +169,7 @@ export default class Board extends Component<props> {
       <div id="boardContainer">
         <svg
           onPointerMove={this.overlayPiece}
+          onMouseOver={this.overlayPiece}
           onClick={this.placePiece}
           id="Board"
           width={sectionSize * gameWidth + margin * 2}
